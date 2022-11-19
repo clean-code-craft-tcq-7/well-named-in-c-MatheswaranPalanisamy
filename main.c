e79cc457e8da1ce69df5937b85cbed7818a75bf2
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-11-19 18:46:27 +0530

    Update main.c to generate color code documentation

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "color_pair_config.h"
 #include "color_pair_test_checker.h"
+#include "color_code_documenter.h"
 
 int main() {
+    DocumentColorCode();
+
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
