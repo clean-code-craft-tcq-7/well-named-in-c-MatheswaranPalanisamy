390e9c50f0ac83328285e487641bfbf0c6aa6266
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-11-19 17:46:49 +0530

    Add color_pair_test_checker module to provide interface to test the functionality

---
+++ color_pair_test_checker.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "color_pair_config.h"
+#include "color_pair_service.h"
+#include "color_pair_test_checker.h"
+
+void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
